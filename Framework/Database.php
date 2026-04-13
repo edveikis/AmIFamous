612f830d958c0217a877c4fbfcdce2ed6c181e4c
Edvinas Bureika <edvinasbureika@gmail.com>
2026-04-13 20:28:59 +0300

    database class

---
+++ Framework/Database.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Framework;
+
+use PDO;
+use Exception;
+use PDOException;
+
+class Database
+{
+    public $conn;
+
+    /**
+     * Constructor
+     * 
+     * @param array $config
+     */
+    public function __construct($config)
+    {
+        $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};";
+
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ];
+
+        try {
+            $this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
+        } catch (PDOException $e) {
+            throw new Exception("Database connection failed: " . $e->getMessage());
+        }
+    }
+
+    /**
+     * Query the database
+     * 
+     * @param string $query
+     * 
+     * @return PDOStatement
+     * @throws PDOException
+     */
+    public function query($query, $params = [])
+    {
+        try {
+            $sth = $this->conn->prepare($query);
+
+            // bind named params
+            foreach ($params as $param => $value) {
+                $sth->bindValue(':' . $param, $value);
+            }
+
+            $sth->execute();
+            return $sth;
+        } catch (PDOException $e) {
+            throw new Exception("Database query failed: " . $e->getMessage());
+        }
+    }
+}
