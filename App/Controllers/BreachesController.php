7b00271ea7f77a22033e01da9ca787e8dd26b5c6
Edvinas Bureika <edvinasbureika@gmail.com>
2026-04-19 21:13:54 +0300

    load search results

--- App/Controllers/BreachesController.php
+++ App/Controllers/BreachesController.php
@@ -2,10 +2,49 @@
 
 namespace App\Controllers;
 
+use Framework\Database;
+
 class BreachesController
 {
+    protected $db;
+
+    public function __construct()
+    {
+        $config = require basePath('config/db.php');
+        $this->db = new Database($config);
+    }
+
     public function getBreaches()
     {
-        echo 'info about the breach page';
+        $email = isset($_POST['email']) ? $_POST['email'] : null;
+
+        if (!$email || empty($email)) {
+            redirect('/');
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // TODO: error message
+            echo "Invalid email format";
+            exit;
+        }
+
+        $breaches = $this->db->query(
+            "SELECT 
+            b.name AS breach_name,
+            br.email,
+            br.username,
+            br.password,
+            br.raw_data_json
+        FROM breach_records br
+        JOIN breaches b ON br.breach_id = b.id
+        WHERE br.email = :email",
+            ['email' => $email]
+        )->fetchAll();
+
+        loadView('breaches', [
+            'breaches' => $breaches,
+            'email' => $email,
+            'breach_count' => count($breaches)
+        ]);
     }
 }
