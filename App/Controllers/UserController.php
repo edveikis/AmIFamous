adfbd3565b82260bb61706a0297bd1c51478f05b
Edvinas Bureika <edvinasbureika@gmail.com>
2026-05-16 22:47:16 +0300

    load login view

---
+++ App/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+use Framework\Database;
+use Framework\Validation;
+use Framework\Session;
+
+class UserController
+{
+    protected $db;
+
+    public function __construct()
+    {
+        $config = require basePath('config/db.php');
+        $this->db = new Database($config);
+    }
+
+    public function login()
+    {
+        return loadView('users/login');
+    }
+}
--- routes.php
+++ routes.php
@@ -2,8 +2,11 @@
 
 $router->get('/', 'HomeController@index');
 
-$router->get('/dashboard', 'DashboardController@index');
-$router->get('/dashboard/import/{file}', 'DashboardController@importForm');
-$router->post('/dashboard/import', 'DashboardController@add');
+$router->get('/dashboard', 'DashboardController@index', ['auth']);
+$router->get('/dashboard/import/{file}', 'DashboardController@importForm', ['auth']);
+$router->post('/dashboard/import', 'DashboardController@add', ['auth']);
 
 $router->post('/breaches', 'BreachesController@getBreaches');
+
+$router->get('/admin/login', 'UserController@login', ['guest']);
+$router->post('/admin/login', 'UserController@authenticate', ['guest']);
