baf2f7b32c150948f89d10c0ed3d6fa50cbb2a14
Edvinas Bureika <edvinasbureika@gmail.com>
2026-06-03 01:38:27 +0300

    csv importer transaction

--- Framework/Import/CSVImporter.php
+++ Framework/Import/CSVImporter.php
@@ -2,6 +2,7 @@
 
 namespace Framework\Import;
 
+use Exception;
 use Framework\Database;
 
 class CSVImporter
@@ -25,45 +26,56 @@ class CSVImporter
 
         $header = ($line = fgetcsv($file));
 
-        $this->db->query(
-            'INSERT INTO breaches(name, file_name) VALUES(:name, :file_name)',
-            [
-                'name' => $fields['name'],
-                'file_name' => $fields['file_name']
-            ]
-        );
+        $this->db->conn->beginTransaction();
 
-        $breachID = $this->db->conn->lastInsertId();
-        $lineCount = 0;
+        try {
 
-        while (($line = fgetcsv($file)) !== FALSE) {
-            $line = array_combine($header, $line);
-
-            $known = array_flip([$fields['email'] ?? '', $fields['username'] ?? '', $fields['password'] ?? '']);
-            $rawData = array_diff_key($line, $known);
 
             $this->db->query(
-                "INSERT INTO breach_records(breach_id, email, username, password, raw_data) VALUES(:breach_id, :email, :username, :password, :raw_data)",
+                'INSERT INTO breaches(name, file_name) VALUES(:name, :file_name)',
                 [
-                    'breach_id' => $breachID,
-                    'email' => $line[$fields['email']],
-                    'username' => $line[$fields['username']] ?? '',
-                    'password' => $line[$fields['password']] ?? '',
-                    'raw_data' => !empty($rawData) ? json_encode($rawData) : null,
-
+                    'name' => $fields['name'],
+                    'file_name' => $fields['file_name']
                 ]
             );
 
-            $lineCount++;
-        }
+            $breachID = $this->db->conn->lastInsertId();
+            $lineCount = 0;
 
-        $this->db->query(
-            'UPDATE breaches SET line_count=:line_count WHERE id=:id',
-            [
-                'line_count' => $lineCount,
-                'id' => $breachID
-            ]
-        );
+            while (($line = fgetcsv($file)) !== FALSE) {
+                $line = array_combine($header, $line);
+
+                $known = array_flip([$fields['email'] ?? '', $fields['username'] ?? '', $fields['password'] ?? '']);
+                $rawData = array_diff_key($line, $known);
+
+                $this->db->query(
+                    "INSERT INTO breach_records(breach_id, email, username, password, raw_data) VALUES(:breach_id, :email, :username, :password, :raw_data)",
+                    [
+                        'breach_id' => $breachID,
+                        'email' => $line[$fields['email']],
+                        'username' => $line[$fields['username']] ?? '',
+                        'password' => $line[$fields['password']] ?? '',
+                        'raw_data' => !empty($rawData) ? json_encode($rawData) : null,
+
+                    ]
+                );
+
+                $lineCount++;
+            }
+
+            $this->db->query(
+                'UPDATE breaches SET line_count=:line_count WHERE id=:id',
+                [
+                    'line_count' => $lineCount,
+                    'id' => $breachID
+                ]
+            );
+        } catch (Exception $e) {
+            // if there is a failure db remains unchanged
+            $this->db->conn->rollBack();
+            fclose($file);
+            throw $e;
+        }
 
         fclose($file);
     }
